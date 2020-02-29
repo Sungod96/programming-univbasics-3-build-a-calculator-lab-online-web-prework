7b004e5cb28b85d6c37e358c103e2737b7e25baf
Amon-Ra Sanders <mrlive2k@gmail.com>
2020-02-29 21:10:11 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,43 +1,41 @@
 def addition(num1,num2)
-  num1 = 5
-  num2 = 4
-  addition = num1 + num2
-  return addition
-
+num1 = 5
+num2 = 8 
+addition = num1 + num2
+return addition
 end
 
 def subtraction(num1, num2)
-  num1 = 10 
-  num2 = 5
-  subtraction = num1 - num2 
-  return subtraction
- 
+ num1 = 10 
+ num2 = 5
+ subtraction = num1 - num2
+ return subtraction
 end
 
 def division(num1, num2)
-  num1 = 50
-  num2 = 2 
+  num1 = 15
+  num2 = 5 
   division = num1/num2
   return division
-  
-
 end
 
 def multiplication(num1, num2)
-num1 = 4
-num2 = 30
-multiplication = num1 * num2
-return multiplication
+  num1 = 10
+  num2 = 5 
+  multiplication = num1 * num2
+  return multiplication
 end
 
 def modulo(num1, num2)
-  num1 = 34
-  num2 = 5
+  num1 = 36
+  num2 = 5 
   modulo = num1 % num2
   return modulo
+  
 end
 
 def square_root(num)
-  Math.sqrt(num)
-  return Math.sqrt(num)
+ Math.sqrt(num)
+ return Math.sqrt(num)
+ 
 end
